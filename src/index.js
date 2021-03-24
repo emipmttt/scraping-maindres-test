31f7739fac6e6f275189d764405f1dfadb7fd332
Max23Esau <esa-max23@live.com>
2021-03-24 00:10:48 -0600

    add mariarivolta

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const shop_lazarocuero = require('./shops/lazarocuero');
 const shop_domaleather = require('./shops/domaleather');
 const shop_aeropostale = require('./shops/aeropostale');
 const shop_airborn = require('./shops/airborn');
+const shop_mariarivolta = require('./shops/mariarivolta');
 
 (async (event, context, callback) => {
   let result = 'result';
@@ -38,6 +39,7 @@ const shop_airborn = require('./shops/airborn');
     const dateScraping = 1615399414960;
 
     // scrapings nuevos
+    await shop_mariarivolta(page, dateScraping);
     await shop_airborn(page, dateScraping);
     await shop_aeropostale(page, dateScraping);
 
